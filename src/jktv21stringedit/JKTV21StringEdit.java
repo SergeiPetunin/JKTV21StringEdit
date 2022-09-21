0e36520d693be99661d6322d732e9e37bda47b74
SergeiPetunin <spetunin@bk.ru>
2022-09-21 19:29:36 +0300

    end task

--- src/jktv21stringedit/JKTV21StringEdit.java
+++ src/jktv21stringedit/JKTV21StringEdit.java
@@ -28,12 +28,11 @@ public class JKTV21StringEdit {
         System.out.println("2 Вывести количество слов: ");
         System.out.println("3 Найти слово в строке: ");
         System.out.println("4 Заменить указанное слово в строке: ");
-        
         int number = scanner.nextInt();
+        scanner.nextLine();
         if(number == 0){
             System.out.println("Vqhod");
             break;
-            
         }else if (number == 1){
             System.out.println("Vqbrali punkt " + number);
             System.out.println("Количество символов " + str.length());
@@ -42,13 +41,33 @@ public class JKTV21StringEdit {
             String[] words = str.split(" ");
             System.out.println("Количество слов " + words.length);
         }else if ( number == 3){
-            System.out.println("Vqbrali punkt " + number);
+            System.out.println("Вы выбрали пункт "+number);
+            System.out.println("Какое слово найти: ");
+            String subStr = scanner.nextLine();
+            System.out.println(subStr);
+            int index = str.indexOf(subStr);
+            if(index < 0){
+                System.out.println("Такого слова нет в строке");
+            }else{
+                index++;
+                System.out.println("Указанное слово находится на "+index+" позиции");
+            }
         }else if (number == 4){
             System.out.println("Vqbrali punkt " + number);
+            System.out.println(str);
+            System.out.println("Введите именяемую строку: ");
+            String editStr = scanner.nextLine();
+            String newStr;
+            int index = str.indexOf(editStr);
+            if(index < 0) {
+                System.out.println("Такой строки в тескте нет");
+            }else{
+                System.out.println("На какую строку заменить: ");
+                newStr = scanner.nextLine();
+                System.out.println("Измененная строка выглядит так: ");
+                System.out.println(str.replace(editStr, newStr));
+            }  
         }
-        
         }while(true);
-        
     }
-    
 }
