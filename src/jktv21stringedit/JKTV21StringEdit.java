f66b34fa9a9110e79d9b7437fc73eb34afdc49f1
pupil <pupil@ivkhk.ee>
2022-09-21 11:20:54 +0300

    interface

--- src/jktv21stringedit/JKTV21StringEdit.java
+++ src/jktv21stringedit/JKTV21StringEdit.java
@@ -21,10 +21,33 @@ public class JKTV21StringEdit {
         System.out.println("-----Введите текст-----");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        System.out.println("kol simv " + str.length());
-        String[] words = str.split(" ");
-        System.out.println("kol slov " + words.length);
+       do{
+        System.out.println("Выберете номер функции: ");
+        System.out.println("0 Выход: ");
+        System.out.println("1 Вывести количество символов: ");
+        System.out.println("2 Вывести количество слов: ");
+        System.out.println("3 Найти слово в строке: ");
+        System.out.println("4 Заменить указанное слово в строке: ");
         
+        int number = scanner.nextInt();
+        if(number == 0){
+            System.out.println("Vqhod");
+            break;
+            
+        }else if (number == 1){
+            System.out.println("Vqbrali punkt " + number);
+            System.out.println("Количество символов " + str.length());
+        }else if (number == 2){
+            System.out.println("Vqbrali punkt " + number);
+            String[] words = str.split(" ");
+            System.out.println("Количество слов " + words.length);
+        }else if ( number == 3){
+            System.out.println("Vqbrali punkt " + number);
+        }else if (number == 4){
+            System.out.println("Vqbrali punkt " + number);
+        }
+        
+        }while(true);
         
     }
     
